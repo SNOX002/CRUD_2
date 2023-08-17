8072334994a76a4b5dbc0361616b37e9c45bcff5
SNOX002 <robertramoncaieiras1@gmail.com>
2023-08-16 22:32:57 -0300

    Milésima tentativa de subir a versão com ADD da FUNC #DELETE

--- src/books.route.js
+++ src/books.route.js
@@ -1,14 +1,13 @@
 const express = require("express")
  
-const allTodos = [{nome: "aaaa", status: false}]
-const todosRoutes = express.Router()
+const booksRoutes = express.Router()
 const { PrismaClient} = require ("@prisma/client")
 
 const prisma = new PrismaClient()
 
 
 // C
-todosRoutes.post("/todos", async (req, res) => {
+booksRoutes.post("/books", async (req, res) => {
     const {name} = req.body
     //Aqui é definido tudo o que será passado como parâmetro
     const todo = await prisma.todo.create({
@@ -16,18 +15,18 @@ todosRoutes.post("/todos", async (req, res) => {
         name,
       }
     })
-    // allTodos.push({name, status: false})
+    // allbooks.push({name, status: false})
     return res.status(201).json(todo)
 })
 // R 
-todosRoutes.get("/todos", async (req, res) => {
+booksRoutes.get("/books", async (req, res) => {
     //Se erra o nome da variavel o método findMany fica inutilizavel
     const todo = await prisma.todo.findMany()
     return res.status(200).json(todo)
 })
 // U 
-todosRoutes.put("/todos", async(req, res) =>{
-  const {name, id} = req.body
+booksRoutes.put("/books", async(req, res) =>{
+  const {name, id, status} = req.body
 
   if(!id)
   {
@@ -51,30 +50,7 @@ todosRoutes.put("/todos", async(req, res) =>{
 })
 
 // D 
-todosRoutes.delete("/todos",async (req, res) => {
-  const {name, id} = req.body
-
-  if(!id)
-  {
-    return res.status(403).json("ID é necessário")
-  }
-  const todoAlreadyExist = await prisma.todo.findUnique({ where: {id} })
-
-  if(!todoAlreadyExist) {
-    return res.status(404).json("Todo Não existe")
-  }
-  const todo = await prisma.todo.delete({ 
-    where: {
-      id, 
-      name,
-      } 
-    })
-    const response = {
-      message: `Todo com ID ${todo.id} foi deletada do DB`,
-      deletedTodo: todo,
-    };
-  return res.status(201).json( response);
-})
 
 
-module.exports = todosRoutes
+
+module.exports = booksRoutes
